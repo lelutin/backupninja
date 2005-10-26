d076494a6ea20754841582d0903b13eb6a973cfd
Micah Anderson <micah@riseup.net>
2005-10-26 23:05:26 +0000

    A little rearranging to get to autotools

---
+++ autogen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "x$1" = "x-f"  ]
+then
+    autoscan
+    [ -f "configure.in" ] && cp "configure.in" "configure.in.old"
+    mv -f "configure.scan" "configure.in"
+    echo "## This is just AUTOSCAN draft of configure.in"
+    $EDITOR "configure.in"
+fi
+
+### použít jen když je třeba použít configure.h.in
+#autoheader
+
+aclocal
+automake -a
+autoconf
